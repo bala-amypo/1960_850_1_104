be51ec7334420194ad5f95ba71c30ff3b538b517
bala-amypo <bala@amypo.in>
2025-12-22 00:51:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth/")
+@RequestMapping("/api/auth")
 @Tag(name = "Authentication", description = "User authentication endpoints")
 public class AuthController {
 
